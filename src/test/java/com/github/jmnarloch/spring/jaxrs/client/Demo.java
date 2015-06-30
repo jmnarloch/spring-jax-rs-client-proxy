f340b4f1b0e4c817660dddaed3fa3cd3ebd6ca39
Jakub Narloch <jmnarloch@gmail.com>
2015-07-01 00:42:46 +0200

    Updated the documentation

--- src/test/java/com/github/jmnarloch/spring/jaxrs/client/Demo.java
+++ src/test/java/com/github/jmnarloch/spring/jaxrs/client/Demo.java
@@ -16,6 +16,7 @@
 package com.github.jmnarloch.spring.jaxrs.client;
 
 import com.github.jmnarloch.spring.jaxrs.client.annotation.EnableJaxRsClient;
+import com.github.jmnarloch.spring.jaxrs.client.annotation.ServiceUrlProvider;
 import com.github.jmnarloch.spring.jaxrs.client.resteasy.EnableRestEasyClient;
 import com.github.jmnarloch.spring.jaxrs.client.support.JaxRsClientConfigurer;
 import com.github.jmnarloch.spring.jaxrs.client.support.ProviderRegistry;
@@ -25,6 +26,7 @@ import org.jboss.resteasy.plugins.interceptors.encoding.GZIPEncodingInterceptor;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
